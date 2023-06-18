c926e1fffa3a2fb7ee973b2c66c28de99c80fa37
jsrushing <jsrushing@basicpointit.com>
2023-06-17 23:15:07 -0400

    PIN management on frmSelectNotebooksToSearch is working flawlessly :).

--- subforms/frmSelectNotebooksToSearch.cs
+++ subforms/frmSelectNotebooksToSearch.cs
@@ -5,8 +5,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using myNotebooks.objects;
+using Org.BouncyCastle.Asn1.X509;
 
 namespace myNotebooks.subforms
 {
@@ -46,23 +48,42 @@ namespace myNotebooks.subforms
 
 			Utilities.SetStartPosition(this, parent);
 		}
-
-		private void AddHasPINTIndicators()
+		
+		private async Task AddHasPINIndicators()
 		{
-			for(int i = 0; i < lstJournalPINs.Items.Count; i++)
+			if (Program.DictCheckedNotebooks.Count > 0)
 			{
+				// JSR : 60/17/23 - Make this more LINQ'd up ...
+				//List<string> v = lstJournalPINs.Items.OfType<string>().ToList().Intersect(Program.DictCheckedNotebooks.Keys).ToList();
+				
+				try 
+				{
+					//var v2 = Program.DictCheckedNotebooks.ToList();  //.ToDictionary(x => x.Value.Length > 0); 
 
+					foreach (KeyValuePair<string, string> kvp in Program.DictCheckedNotebooks)
+					{
+						var name = kvp.Key;
+						var pin = kvp.Value;
+						var newentry = lstJournalPINs.Items.IndexOf(name) > -1 ? lstJournalPINs.Items.IndexOf(name) : lstJournalPINs.Items.IndexOf(name + " (****)");
+
+						if (pin.Length > 0)
+						{ lstJournalPINs.Items[newentry] = name + " (****)"; }
+						else { lstJournalPINs.Items[newentry] = name; }
+					}
+				}
+				catch(Exception ex) { using (frmMessage frm = new frmMessage(frmMessage.OperationType.Message, ex.Message, "Error", this)); }
 			}
 		}
 
-		private void btnAddPIN_Click(object sender, EventArgs e)
+		private async void btnAddPIN_Click(object sender, EventArgs e)
 		{
 			var s = lstJournalPINs.Text.Replace(" (****)", "");
 			var itemIndex = lstJournalPINs.SelectedIndex;
-			dictNotebooksAndPINs[s] = txtPIN.Text;
-			s += txtPIN.Text.Length == 0 ? "" : " (****)";
-			lstJournalPINs.Items.Insert(lstJournalPINs.SelectedIndex, s);
-			lstJournalPINs.Items.RemoveAt(lstJournalPINs.SelectedIndex);
+			Program.DictCheckedNotebooks[s] = txtPIN.Text;
+			//s += txtPIN.Text.Length == 0 ? "" : " (****)";
+			await AddHasPINIndicators();
+			//lstJournalPINs.Items.Insert(lstJournalPINs.SelectedIndex, s);
+			//lstJournalPINs.Items.RemoveAt(lstJournalPINs.SelectedIndex);
 			txtPIN.PasswordChar = '\0';
 			txtPIN.Text = "(select a Journal)";
 			txtPIN.Enabled = false;
@@ -77,19 +98,22 @@ namespace myNotebooks.subforms
 		private void btnDone_Click(object sender, EventArgs e)
 		{
 			string[] checkedItems = lstJournalPINs.CheckedItems.OfType<string>().ToArray();
-			for (var i = 0; i < checkedItems.Length; i++) { checkedItems[i] = Scrubbed(checkedItems[i]); }
-			Dictionary<string, string> tmpDict = dictNotebooksAndPINs;
 
-			foreach (KeyValuePair<string, string> kvp in tmpDict)
-			{ if (!checkedItems.Contains(kvp.Key)) { tmpDict.Remove(kvp.Key); } }
+
+
+			for (var i = 0; i < checkedItems.Length; i++) { checkedItems[i] = Scrubbed(checkedItems[i]); }
+			//Dictionary<string, string> tmpDict = dictNotebooksAndPINs;
+
+			foreach (KeyValuePair<string, string> kvp in Program.DictCheckedNotebooks)
+			{ if (!checkedItems.Contains(kvp.Key)) { Program.DictCheckedNotebooks.Remove(kvp.Key); } }
 
 			foreach (var item in checkedItems)
 			{
-				if (!tmpDict.ContainsKey(Scrubbed(item)))
-				{ dictNotebooksAndPINs.Add(Scrubbed(item), ""); }
+				if (!Program.DictCheckedNotebooks.ContainsKey(Scrubbed(item)))
+				{ Program.DictCheckedNotebooks.Add(Scrubbed(item), ""); }
 			}
 
-			Program.DictCheckedNotebooks = dictNotebooksAndPINs;
+			//Program.DictCheckedNotebooks = dictNotebooksAndPINs;
 			this.Hide();
 		}
 
@@ -139,55 +163,34 @@ namespace myNotebooks.subforms
 		{
 			if(!showMore) lstJournalPINs.Items.Clear();
 
-			if(showAll)
+			lstJournalPINs.Items.Clear();
+
+			if (showAll)
 			{
 				await Utilities.PopulateAllNotebookNames();
 
-				foreach (var notebookName in Program.AllNotebookNames)
-				{
-					if (Program.DictCheckedNotebooks.ContainsKey(notebookName))
-					{
-						if (Program.DictCheckedNotebooks[notebookName].Length > 0)
-						{
-							lstJournalPINs.Items.Add(notebookName + " (****)");
-							dictNotebooksAndPINs.Add(notebookName, Program.DictCheckedNotebooks[notebookName]);
-						}
-						else
-						{
-							lstJournalPINs.Items.Add(notebookName);
-							dictNotebooksAndPINs.Add(notebookName, "");
-						}
+				//lstJournalPINs.Items.AddRange(Program.AllNotebookNames)
 
-						lstJournalPINs.SetItemChecked(lstJournalPINs.Items.Count - 1, true);
-					}
-					else { lstJournalPINs.Items.Add(notebookName); }
-				}
+				foreach (var notebookName in Program.AllNotebookNames) { lstJournalPINs.Items.Add(notebookName); }
 			}
 
-			if (populateWithCheckedJournals)
-			{
-				//object[] objects = (object[])Program);
-				//lstJournalPINs.Items.AddRange(objects);
-
-				foreach (var name in Program.DictCheckedNotebooks.Keys) { lstJournalPINs.Items.Add(Program.DictCheckedNotebooks[name].Length > 0 ? name + " (****)" : name); }
-				for (var i  = 0; i < lstJournalPINs.Items.Count; i++) { lstJournalPINs.SetItemChecked(i, true); }
-				lstJournalPINs.Items.Add(ShowMoreString);
-			}
+			if (populateWithCheckedJournals) { PopulateCheckedItems(); }
 
 			if (showMore)
 			{
+				PopulateCheckedItems();
 				lstJournalPINs.Items.Remove(ShowMoreString);
 				foreach(var name in Program.AllNotebookNames.Except(Program.DictCheckedNotebooks.Keys)) { lstJournalPINs.Items.Add($"{name}"); }
-
-				//string[] names = Program.AllNotebookNames.ToArray();
-				//List<string> lst = Program.AllNotebookNames.ToList();
-				//IEnumerable<string> strings = Program.AllNotebookNames.Except(Program.DictCheckedNotebooks.Keys);
-				//List <KeyValuePair<string, string>> items = Program.AllNotebookNames.Except(Program.DictCheckedNotebooks.Keys).ToDictionary(x => x, x => x).ToList();
-
-				//object[] objects = (object[])Program.AllNotebookNames.Except(Program.DictCheckedNotebooks.Keys);
-				//List<KeyValuePair<string, string>> lst = new List<KeyValuePair<string, string>>(Program.AllNotebookNames.Except(Program.DictCheckedNotebooks.Keys), "");
-				//lstJournalPINs.Items.AddRange(objects);
 			}
+
+			AddHasPINIndicators();
+		}
+
+		private void PopulateCheckedItems()
+		{
+			foreach (var name in Program.DictCheckedNotebooks.Keys) { lstJournalPINs.Items.Add(name); }
+			for (var i = 0; i < lstJournalPINs.Items.Count; i++) { lstJournalPINs.SetItemChecked(i, true); }
+			lstJournalPINs.Items.Add(ShowMoreString);
 		}
 
 		private string Scrubbed(string stringToScrub) { return stringToScrub.Replace(" (****)", ""); }
